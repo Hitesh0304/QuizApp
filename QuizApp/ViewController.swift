3444cc7f00320274cb82ab08d39f238dd6dbc496
Hitesh Moudgil <200378502@student.georgianc.on.ca>
2023-10-06 08:35:08 -0400

    created UI

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Hitesh Moudgil on 2023-10-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
