15cd0e74b783707d290eec195da4453d77b6c70a
Hitesh Moudgil <200378502@student.georgianc.on.ca>
2023-10-06 20:48:12 -0400

    implemented MVC pattern

---
+++ QuizApp/Controller/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Hitesh Moudgil on 2023-10-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: IB OUTLETS
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    //MARK: Instantiate QuizBrain
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle! //True, False
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+                
+        if(userGotItRight){
+            sender.backgroundColor = UIColor.green
+        }
+        else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.25, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        
+        trueButton.backgroundColor = UIColor.systemCyan
+        falseButton.backgroundColor = UIColor.systemCyan
+    }
+}
+
